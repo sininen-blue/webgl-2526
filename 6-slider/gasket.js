30d3d5c3241a2b4c4896da156f92ab9c0e93b24f
sininen-blue <alaanneil@gmail.com>
2025-09-04 10:27:01 +0800

    feat: added sliders

---
+++ 6-slider/gasket.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var positions = [];
+var numPositions;
+
+var slider = document.getElementById("depth");
+
+window.onload = function() {
+	initGL();
+}
+
+slider.oninput = function() {
+	numPositions = parseInt(this.value);
+	positions = [];
+
+	// step 3.1
+	// set up the triangle
+	var vertices = [
+		vec2(-1, -1),
+		vec2(0, 1),
+		vec2(1, -1)
+	];
+
+	// step 3.2
+	// Pick an initial point inside the triangle
+
+	// step 4
+	// generate the sierpinski gasket
+	divideTriangle(vertices[0], vertices[1], vertices[2], numPositions);
+
+	LoadDataToGPU(positions);
+	// NOTE: what is this function doing?
+	// the code for this is located at the js/webgl.js file, in the loadDataTOGPU() function
+	// describe the code in a high level, step by step
+
+	render();
+	// NOTE: What type of rendering are we doing here? Justify your answer (immediate or retained mode or modern)
+}
+
+function divideTriangle(a, b, c, count) {
+	if (count == 0) {
+		triangle(a, b, c)
+	} else {
+		var ab = mix(a, b, 0.5);
+		var ac = mix(a, c, 0.5);
+		var bc = mix(b, c, 0.5);
+
+		count--;
+
+		divideTriangle(a, ab, ac, count);
+		divideTriangle(c, ac, bc, count);
+		divideTriangle(b, bc, ab, count);
+	}
+}
+
+function triangle(a, b, c) {
+	positions.push(a)
+	positions.push(b)
+	positions.push(c)
+}
